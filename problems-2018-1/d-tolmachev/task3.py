2a4219d2bbd43baeebf244df913c06122f9f87dd
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2023-03-13 10:17:11 +0700

    Problems-2018-1. Task3 fixed

--- problems-2018-1/d-tolmachev/task3.py
+++ problems-2018-1/d-tolmachev/task3.py
@@ -11,15 +11,18 @@ def lprimes(n: int) -> list[int]:
     primes: list[boolean] = [True if i >= 2 else False for i in range(0, n + 1)]
     for k in range(2, math.ceil(math.sqrt(n))):    # O(NloglogN)
         if primes[k]:
-            for i in range(k * 2, n, k):
-                primes[i] = False
+            primes[k * 2 : n : k] = [False] * ((n - k * 2 - 1) // k + 1)
     return [i for i, _ in filter(lambda entry: entry[1], enumerate(primes))]
 
 # Generates prime numbers less than the specified number using sets
 def sprimes(n: int) -> list[int]:
-    numbers: set(int) = {i for i in range(2, n)}
-    not_primes: set[int] = {i for k in range(2, math.ceil(math.sqrt(n))) for i in range(k * 2, n, k)}    # O(NlogN)
-    return list(numbers - not_primes)
+    not_primes: set[int] = set()
+    primes: list[int] = []
+    for k in range(2, math.ceil(math.sqrt(n))):    # O(NloglogN)
+        if k not in not_primes:
+            not_primes.union({i for i in range(k * 2, n, k)})
+            primes.append(k)
+    return primes
 
 # Generates prime numbers less than the specefied number using bitarray
 def bprimes(n: int) -> list[int]:
@@ -32,17 +35,17 @@ def bprimes(n: int) -> list[int]:
 
 # Measures the execution time of a given function
 def timeit(function: Callable[[int], list[int]], *args: tuple[Any], number: int = 16) -> float:
-    durations: list[float] = []
+    durations: float = 0.0
     for _ in range(number):    # Multiple cycles for more accurate results
         start: float = time.time()
         function(args[0])
         end: float = time.time()
-        durations.append(1000 * datetime.timedelta(seconds = end - start).total_seconds())
-    return sum(durations) / len(durations)
+        durations += 1000 * datetime.timedelta(seconds = end - start).total_seconds()
+    return durations / number
 
 # Used as entrypoint
 if __name__ == '__main__':
     n: int = 100000000
-    print(f'List: {timeit(bprimes, n)}')
-    print(f'Set: {timeit(bprimes, n)}')
-    print(f'Bitarray: {timeit(bprimes, n)}')
+    primes_functions: dict[str, Callable[[int], list[int]]] = {'list': lprimes, 'set': sprimes, 'bitarray': bprimes}
+    for function_type, primes_function in primes_functions.items():
+        print(f'{function_type.title()}: {timeit(primes_function, n, number = 1):.3g}')
